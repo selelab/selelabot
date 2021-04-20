a1bce6769cb2a587de434aff6f96067faf7a7c3c
stepney141 <stepney141andi.c.i@gmail.com>
2021-04-20 17:15:49 +0900

    引数の取り方・メンションの飛ばし方

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as Discord from 'discord.js';
 import fs from 'fs';
 
 const config = JSON.parse(fs.readFileSync('./env.json')); //設定ファイルの読み込み
-const prefix = config.PREFIX;
+const { token, prefix } = config;
 const client = new Discord.Client(); // Discordクライアントの作成
 
 client.once('ready', () => {
@@ -10,19 +10,31 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    if (!message.author.bot && message.content.charAt(0) === prefix) {
-        if (message.content === `${prefix}ping`) {
-            message.channel.send('Pong.');
-        } else if (message.content === `${prefix}pingu`) {
-            message.channel.send('Pongu.');
-        } else if (message.content === `${prefix}servername`) {
-            message.channel.send(`This server's name is: ${message.guild.name}`);
-        } else if (message.content === `${prefix}server`) {
-            message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-        } else if (message.content === `${prefix}userinfo`) {
-            message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-        } else { message.channel.send('Invalid command.'); }
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') { message.channel.send('Pong.'); }
+    else if (command === 'pingu') { message.channel.send('Pongu'); }
+    else if (command === 'userinfo') { message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`); }
+    else if (command === 'serverinfo') { message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`); }
+    else if (command === 'server') {
+        if (args[0] === 'name') message.channel.send(`Server name: ${message.guild.name}`);
+        else if (args[0] === 'member') message.channel.send(`Total members: ${message.guild.memberCount}`);
+        else if (!args[0]) message.channel.send('No arguments.');
+        else message.channel.send('Invalid argument.');
     }
+    else if (command === 'kick') {
+        // grab the "first" mentioned user from the message
+        // this will return a `User` object, just like `message.author`
+        if (!message.mentions.users.size) {
+            return message.reply('you need to tag a user in order to kick them!');
+        }
+        const taggedUser = message.mentions.users.first();
+        message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    }
+    else { message.channel.send('Invalid command.'); }
 });
 
-client.login(config.TOKEN); // ログイン
+client.login(token); // ログイン
