2cba5c9b5dfcee01d23089cda1592e1cda53b300
stepney141 <stepney141andi.c.i@gmail.com>
2021-04-25 05:28:19 +0900

    設定方法の調整

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 const fs = require('fs');
 
 const config = JSON.parse(fs.readFileSync('./setting/env.json')); //設定ファイルの読み込み
-const grade_list = require('./setting/selelab.json');
+const server_setting = require('./setting/selelab.json');
 
 const { token, prefix, guild_id } = config;
-const client = new Discord.Client({
+const client = new Discord.Client({ //Discordクライアントの作成
     ws: {
         intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS', 'GUILD_PRESENCES'] //Gateway Intentの有効化・指定
     }
-}); // Discordクライアントの作成
+});
 
 /* 一定時間だけ非同期で処理を待つ(単位：秒) */
 const sleep = async (seconds) => new Promise((resolve, reject) => { setTimeout(() => { resolve(); }, seconds * 1000); });
@@ -63,7 +63,7 @@ client.on('guildMemberAdd', async (member) => {
     }
 
     /* チャンネル名の完全一致で特定のチャンネルを指定し、そこで役職付与プロトコルを開始 */
-    const welcomeChannel = client.guilds.cache.get(guild_id).channels.cache.find(channel => channel.name === 'テストチャンネルその1');
+    const welcomeChannel = client.guilds.cache.get(guild_id).channels.cache.find(channel => channel.name === server_setting.CHANNEL.WELCOME);
     if (!welcomeChannel) {
         return console.error("該当するチャンネルが見つかりませんでした");
     }
@@ -75,7 +75,7 @@ client.on('guildMemberAdd', async (member) => {
 
     await sleep(2); //2秒待つ
 
-    /* 役職付与 */
+    /* 役職付与プロトコル開始 */
     welcomeChannel.send(`Q. ${member}さん、あなたは何年生ですか？\n
     次の選択肢の中からあなたの現在の学年に対応する番号を選び、その番号を__**半角数字1個だけで**__ここに投稿して下さい！`);
     const gradeEmbed = new Discord.MessageEmbed()
@@ -83,14 +83,15 @@ client.on('guildMemberAdd', async (member) => {
         .setTitle('あなたの現在の学年に対応する番号を投稿して下さい')
         .setDescription('入力するのは半角数字一個だけです。日本語とか記号とかは入力しないで下さい')
         .addFields(
-            { name: `${grade_list.GRADE.FIRST}なら`, value: '1 と入力'},
-            { name: `${grade_list.GRADE.SECOND}なら`, value: '2 と入力' },
-            { name: `${grade_list.GRADE.THIRD}なら`, value: '3 と入力' },
-            { name: `${grade_list.GRADE.FOURTH}なら`, value: '4 と入力' },
-            { name: `${grade_list.GRADE.GRADUATED}なら`, value: '5 と入力' },
+            { name: `${server_setting.GRADE.FIRST}なら`, value: '1 と入力'},
+            { name: `${server_setting.GRADE.SECOND}なら`, value: '2 と入力' },
+            { name: `${server_setting.GRADE.THIRD}なら`, value: '3 と入力' },
+            { name: `${server_setting.GRADE.FOURTH}なら`, value: '4 と入力' },
+            { name: `${server_setting.GRADE.GRADUATED}なら`, value: '5 と入力' },
         );
     welcomeChannel.send(gradeEmbed);
 
+    /* ユーザーの入力を受け取り、処理する */
     const msgFilter = msg => {
         return (msg.author.id === member.id && msg.content.match(/^[12345]$/)) ? true : false;
         //「新しく参加したメンバーの投稿である」かつ「1から5までの半角数字のどれか一つだけを含む」 => 条件に合致
@@ -102,13 +103,13 @@ client.on('guildMemberAdd', async (member) => {
             
             try {
                 const grade_number = Number(collected.first().content);
-                const grade_name = grade_list.ROLE.find(data => data.NUM === grade_number);
+                const grade_name = server_setting.ROLE.find(data => data.NUM === grade_number);
                 const grade_role = member.guild.roles.cache.find(role => role.name === grade_name.GRADE);
 
                 member.roles.add(grade_role); //対応する学年役職を参加メンバーに追加
 
                 /* 別のチャンネルで新規参加者のことをお知らせする */
-                const infoChannel = client.guilds.cache.get(guild_id).channels.cache.find(channel => channel.name === 'テストチャンネルその1');
+                const infoChannel = client.guilds.cache.get(guild_id).channels.cache.find(channel => channel.name === server_setting.CHANNEL.INFO);
                 if (!infoChannel) {
                     return console.error("該当するチャンネルが見つかりませんでした");
                 }
